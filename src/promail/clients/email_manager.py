273c7edafed71711ee476c24360063d7b1ee05f1
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2022-05-07 11:45:13 -0400

    formating chanages

--- src/promail/clients/email_manager.py
+++ src/promail/clients/email_manager.py
@@ -2,12 +2,12 @@ import abc
 
 
 class OutBoundManager(abc.ABC):
-
-    def send_email(self, recipients: str, cc: str, bcc: str, subject: str, htmltext: str, plaintext):
+    def send_email(
+        self, recipients: str, cc: str, bcc: str, subject: str, htmltext: str, plaintext
+    ):
         pass
 
 
 class InBoundManager(abc.ABC):
-
     def retrieve_last_items(self, max_items):
         pass
--- tests/test_outlook_client.py
+++ tests/test_outlook_client.py
@@ -3,16 +3,16 @@ import time
 from promail.clients.outlook import OutLookClient
 from settings import TEST_EMAIL
 
+
 def send_email():
     emailer = OutLookClient()
     text = "<h1>Test Email </h1>"
-    emailer.send_email(TEST_EMAIL, '', '', "Test Email", text, '')
+    emailer.send_email(TEST_EMAIL, "", "", "Test Email", text, "")
     time.sleep(3)
+
+
 def test_outlook():
     try:
         send_email()
     except OSError:
         print("Test Skipped on non windows PC")
-
-
-
